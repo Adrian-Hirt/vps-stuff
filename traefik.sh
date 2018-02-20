fc8eb4b43d59e1cae4def444a8183917dd864592
Adrian Hirt <aedu.hirt@gmail.com>
2018-02-20 09:18:57 +0100

    Let's encrypt is now working
    Updated to traefik 1.5.2-alpine and using the http challenge
    endpoint for let's encrypt because the TLS-SNI-01 protocol is not
    working at the moment

--- traefik.sh
+++ traefik.sh
@@ -1,3 +1,3 @@
 # /bin/bash
 
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/traefik.toml:/traefik.toml -v $PWD/acme.json:/acme.json -p 80:80 -p 443:443 -l traefik.frontend.rule=Host:monitoring.hirt.one -l traefik.port=8080 --network proxy --name traefik traefik:1.3.6-alpine --docker
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/traefik.toml:/traefik.toml -v $PWD/acme.json:/acme.json -p 80:80 -p 443:443 -l traefik.frontend.rule=Host:monitoring.hirt.one -l traefik.port=8080 --network proxy --name traefik traefik:1.5.2-alpine --docker
